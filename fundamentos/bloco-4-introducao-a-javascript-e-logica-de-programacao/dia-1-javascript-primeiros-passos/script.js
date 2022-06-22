842d4bd6fc0d6436ae7cf6635d4e23d69f191387
Luis Alberto H. Cardoso <luis.alberto3556@gmail.com>
2022-06-22 13:58:34 -0300

    resolução dos exercícios do 1 ao 4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,55 @@
+//1° PROGRAMA 
+
+// const a = 150;
+// const b = 245;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+//2° PROGRAMA
+
+// const a = 2626438;
+// const b = 4674738;
+// let maiorNumero;
+
+// if (a > b) {
+//     maiorNumero = a;
+// } else if (a < b) {
+//     maiorNumero = b;
+// }
+// console.log (maiorNumero)
+
+
+//3° PROGRAMA
+
+// const a = 163758;
+// const b = 657483;
+// const c = 774848;
+// let maiorNumero;
+
+// if (a > b) {
+//     maiorNumero = a;
+// } else if (a < b) {
+//     maiorNumero = b;
+// } else if (maiorNumero > c) {
+//     maiorNumero = maiorNumero;
+// } else {
+//     maiorNumero = c;
+// }
+// console.log (maiorNumero)
+
+//4° PROGRAMA
+
+// const positivoNegativo = -1;
+
+// if (positivoNegativo >= 0) {
+//     console.log("positive");
+// } else if (positivoNegativo < 0) {
+//     console.log("negative");
+// }
+
+//5° PROGRAMA
+
