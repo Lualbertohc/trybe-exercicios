5cd44034af7c84d4f804a473211ba19c3ccacf2a
Luis Alberto H. Cardoso <luis.alberto3556@gmail.com>
2022-06-26 13:01:27 -0300

    resolução dos exercícios do 5 ao 7

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -11,45 +11,89 @@
 
 //2° PROGRAMA
 
-// const a = 2626438;
-// const b = 4674738;
-// let maiorNumero;
+// const a = 150;
+// const b = 245;
 
-// if (a > b) {
-//     maiorNumero = a;
-// } else if (a < b) {
-//     maiorNumero = b;
+// if (a > b){
+//     console.log(a);
+// } else {
+//     console.log (b)
 // }
-// console.log (maiorNumero)
-
 
 //3° PROGRAMA
 
-// const a = 163758;
-// const b = 657483;
-// const c = 774848;
-// let maiorNumero;
+// const a = 150;
+// const b = 245;
+// const c = 764;
 
-// if (a > b) {
-//     maiorNumero = a;
-// } else if (a < b) {
-//     maiorNumero = b;
-// } else if (maiorNumero > c) {
-//     maiorNumero = maiorNumero;
+// if (a > b && a > c) {
+//     console.log("O maior número é " + a);
+// } else if (b > a && b > c) {
+//     console.log("O maior número é " + b);
 // } else {
-//     maiorNumero = c;
+//     console.log("O maior número é " + c);
 // }
-// console.log (maiorNumero)
 
 //4° PROGRAMA
 
-// const positivoNegativo = -1;
+// const numero = 25;
 
-// if (positivoNegativo >= 0) {
-//     console.log("positive");
-// } else if (positivoNegativo < 0) {
+// if (numero > 0){
+//     console.log("positive")
+// } else {
 //     console.log("negative");
 // }
 
 //5° PROGRAMA
 
+// const ang1 = 75;
+// const ang2 = 64;
+// const ang3 = 7;
+
+// if (ang1 + ang2 + ang3 === 180)
+
+// 6° PROGRAMA
+
+// function xadrez(peca) {
+//     peca = peca.toLowerCase();
+
+//     if (peca === "peão") {
+//         return ("Move-se sempre uma casa para frente, exceto no primeiro movimento, quando pode mover-se duas casas");
+//     } else {
+//         return("peça inválida");
+//     }
+// }
+// console.log(xadrez("Peão"));
+
+// 7° PROGRAMA
+
+// let faixaEtaria = 'adulto';
+
+// switch (faixaEtaria) {
+//   case 'adolescente':
+//     // Caso a variável seja "adolescente" então entraremos na lógica abaixo.
+
+//     console.log('Consulte a classificação do filme');
+//     // Quando a pessoa for adolescente essa será a mensagem impressa no console.
+
+//     break;
+// default:
+
+let nota = (40/100);
+
+if (nota >= (90/100)) {
+    nota = "A";
+} else if (nota >= (80/100)) {
+    nota = "B";
+} else if (nota >= (70/100)) {
+    nota = "C";
+} else if (nota >= (60/100)) {
+    nota = "D";
+} else if (nota >= (50/100)) {
+    nota = "E";
+} else if (nota < (50/100)) {
+    nota = "F";
+} else if (nota < 0 || nota > (100/100)) {
+    console.log("erro");
+}
+console.log(nota);
