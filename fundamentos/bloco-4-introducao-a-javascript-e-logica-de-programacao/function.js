ef47e19605b9463f47ac9e3c9833bded08a34f03
Luis Alberto H. Cardoso <luis.alberto3556@gmail.com>
2022-06-26 10:35:36 -0300

    exercício 1 função

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/function.js
@@ -0,0 +1,11 @@
+// 1 - Crie uma função que receba uma string e retorne true se for um palíndromo, ou false, se não for.
+
+// a. Exemplo de palíndromo: arara;
+// b. verificaPalindrome('arara');
+// c. Retorno esperado: true;
+// d. verificaPalindrome('desenvolvimento');
+// e. Retorno esperado: false;
+
+// function(palindromo){
+
+// }
